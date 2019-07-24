b0604ccf493c2181c50387af17390633ecb64e87
com <walls.jyron@gmail.com>
2019-07-24 14:19:50 -0700

    initial commit

---
+++ writerbae/main.py
@@ -0,0 +1,14 @@
+#routes for 'not logged in' people
+
+from flask import Blueprint, render_template
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
