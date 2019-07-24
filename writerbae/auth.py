86ddbf53c530586cab792f68768dd3b2ec1f9e93
com <walls.jyron@gmail.com>
2019-07-24 16:26:11 -0700

    route functions

--- writerbae/auth.py
+++ writerbae/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -9,9 +11,33 @@ def login():
 
 @auth.route('/signup')
 def signup():
+    def signup_post():
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
     return render_template('signup.html')
 
 @auth.route('/logout')
 def logout():
     return 'Logout'
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    #login code here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(email=email).first()
+
+    if not user: 
+        flash('We dont have that email. Try again.')
+        return redirect(url_for('auth.login'))
+    elif not check_password_hash(user.password, password):
+        flash('Wrong password {}'.format(email))
+        return redirect(url_for(auth.login))
+    return redirect(url_for('main.profile'))
+
+
+
 
